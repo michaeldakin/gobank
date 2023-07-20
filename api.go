e9e3c4f7ad04f59b5013106c37dfbf3bb438141d
Michael Dakin <m.dakin@outlook.com>
2023-07-20 14:34:38 +1000

    Added gitignore and starting to return user values instead of static data

--- api.go
+++ api.go
@@ -43,10 +43,9 @@ func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
-
 	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccount))
 
-	log.Println("JSON API server running on port:", s.listenAddr)
+	log.Println("JSON API server running on port", s.listenAddr)
 
 	http.ListenAndServe(s.listenAddr, router)
 }
@@ -66,8 +65,11 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	account := NewAccount("Mike", "Dakin")
-	return WriteJSON(w, http.StatusOK, account)
+	id := mux.Vars(r)["id"]
+	// db.get(id) etc
+	//
+	fmt.Println(id)
+	return WriteJSON(w, http.StatusOK, &Account{})
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
